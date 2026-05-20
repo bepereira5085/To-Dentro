54ca56778691dcc1892b5eda5143c5d67ec8bb62
Bernardo Pereira <bernardopcandido@gmail.com>
2026-05-20 20:03:40 -0300

    feat: create recurrences enums

--- to_dentro/models/event_recurrence.py
+++ to_dentro/models/event_recurrence.py
@@ -1,6 +1,8 @@
+import enum
 from typing import List, TYPE_CHECKING
 from datetime import date
 from sqlalchemy import ForeignKey, String, Integer, Date
+from sqlalchemy import ForeignKey, String, Integer, Date, CheckConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from to_dentro.ext.db import db
 
@@ -8,20 +10,31 @@ if TYPE_CHECKING:
     from .event import Event
     from .event_recurrence_weekday import EventRecurrenceWeekday
 
-#  TODO: fazer enum de recurrence
+class RecurrenceTypes(enum.Enum):
+    DAILY = 'Diário'
+    WEEKLY = 'Semanal'
+    MONTHLY = 'Mensal'
+    YEARLY = 'Anual'
+
+class WeeksInterval(enum.Enum):
+    ONE = 1 # intervalo de 1 semana (quinzenal)
+    TWO = 2 # intervalo de 2 semanas (primeira semana - quarta semana)
 
 class EventRecurrence(db.Model):
     __tablename__ = 'event_recurrences'
-    __table_args__ = {'extend_existing': True}
+    __table_args__ = (
+        CheckConstraint('day_of_month >= 1 AND day_of_month <= 31', name='check_day_of_month_range'),
+        {'extend_existing': True}
+    )
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     event_id: Mapped[int] = mapped_column(
         ForeignKey("events.id", ondelete="CASCADE")
     )
-    type: Mapped[str] = mapped_column(String(50))
+    type: Mapped[str] = mapped_column(db.Enum(RecurrenceTypes))
     start_date: Mapped[date] = mapped_column(Date)
     end_date: Mapped[date] = mapped_column(Date, nullable=True)
-    weeks_interval: Mapped[int] = mapped_column(Integer, nullable=True)
+    weeks_interval: Mapped[int] = mapped_column(db.Enum(WeeksInterval), nullable=True)
     day_of_month: Mapped[int] = mapped_column(Integer, nullable=True)
 
     event: Mapped["Event"] = relationship(
--- to_dentro/models/event_recurrence_weekday.py
+++ to_dentro/models/event_recurrence_weekday.py
@@ -1,3 +1,4 @@
+import enum
 from typing import TYPE_CHECKING
 from sqlalchemy import ForeignKey, Integer
 from sqlalchemy.orm import Mapped, mapped_column, relationship
@@ -6,7 +7,14 @@ from to_dentro.ext.db import db
 if TYPE_CHECKING:
     from .event_recurrence import EventRecurrence
 
-# TODO: fazer enum de weekdays
+class WeekDays(enum.Enum):
+    SUNDAY = 1
+    MONDAY = 2
+    TUESDAY = 3
+    WEDNESDAY = 4
+    THURSDAY = 5
+    FRIDAY = 6
+    SATURDAY = 7
 
 class EventRecurrenceWeekday(db.Model):
     __tablename__ = 'event_recurrence_weekdays'
@@ -15,7 +23,7 @@ class EventRecurrenceWeekday(db.Model):
     recurrence_id: Mapped[int] = mapped_column(
         ForeignKey("event_recurrences.id", ondelete="CASCADE"), primary_key=True
     )
-    weekday: Mapped[int] = mapped_column(Integer, primary_key=True)
+    weekday: Mapped[int] = mapped_column(db.Enum(WeekDays), primary_key=True)
 
     recurrence: Mapped["EventRecurrence"] = relationship(
         "EventRecurrence", back_populates="weekdays"
